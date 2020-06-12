12bbbc710d42671aa017a70d74283e35e23426cf
sanket2701 <sanket.bafn@somaiya.edu>
2020-06-12 17:02:10 +0530

    UI changes

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
-  final String product_name;
-  final String product_image;
-  final int product_oldPrice;
-  final int product_newPrice;
+  // ignore: non_constant_identifier_names
+  final String product_name, product_image;
+
+  // ignore: non_constant_identifier_names
+  final int product_oldPrice, product_newPrice;
 
   ProductDetails(this.product_name, this.product_image, this.product_oldPrice,
       this.product_newPrice);
@@ -42,9 +44,178 @@ class _ProductDetailsState extends State<ProductDetails> {
           Container(
             height: 300.0,
             child: GridTile(
-                child: Container(
-              child: Image.asset(widget.product_image),
-            )),
+              child: Container(
+                color: Colors.white,
+                child: Image.asset(widget.product_image),
+              ),
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    widget.product_name,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("₹" + widget.product_oldPrice.toString()),
+                      ),
+                      Expanded(
+                        child: Text("₹" + widget.product_newPrice.toString()),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Size'),
+                            content: Text('Choose the Size'),
+                            actions: <Widget>[
+                              MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text('close'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  elevation: 4.0,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Size')),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Color'),
+                            content: Text('Choose the Color'),
+                            actions: <Widget>[
+                              MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text('close'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  elevation: 4.0,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Color')),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Quantity'),
+                            content: Text('Choose the Quantity'),
+                            actions: <Widget>[
+                              MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text('close'),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  elevation: 4.0,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Qty')),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 4.0,
+                  child: Text('Buy Now'),
+                ),
+              ),
+              IconButton(
+                  icon: Icon(Icons.add_shopping_cart, color: Colors.red,),
+                  onPressed: null
+              ),
+              IconButton(
+                  icon: Icon(Icons.favorite_border, color: Colors.red,),
+                  onPressed: null)
+            ],
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Product Details'),
+            subtitle: Text('Lorem Ipsum'),
+          ),
+          Divider(),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 5.0, 5.0),
+                child: Text('Product Name   ' + widget.product_name),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 5.0, 5.0),
+                child: Text('Product Brand  ' + 'Brand X'),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 5.0, 5.0),
+                child: Text('Product Condition  ' + 'Safe'),
+              )
+            ],
           ),
         ],
       ),
